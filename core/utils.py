b997212790d4dc9dfb8e8928ca4b7acef095d7df
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-03-21 13:37:23 +0100

    feat(core.utils.BasicLinearReg->core.sensor_processing.math.BasicLinearReg): mv

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,6 @@
 import sys
 import glob
 import serial
-import math
 
 
 def serial_ports():
@@ -37,48 +36,3 @@ def detect_arduino_usb_serial():
     for _ in serial_ports():
         if "usbserial" in _:
             return _
-
-
-class BasicLinearReg:
-
-    @staticmethod
-    def mean(xs):
-        return sum(xs) / len(xs)
-
-    @staticmethod
-    def std(xs, m):
-        normalizer = len(xs) - 1
-        return math.sqrt(sum((pow(x - m, 2) for x in xs)) / normalizer)
-
-    @staticmethod
-    def pearson_r(xs, ys, m_x, m_y):
-        sum_xy = 0
-        sum_sq_v_x = 0
-        sum_sq_v_y = 0
-
-        for (x, y) in zip(xs, ys):
-            var_x = x - m_x
-            var_y = y - m_y
-            sum_xy += var_x * var_y
-            sum_sq_v_x += pow(var_x, 2)
-            sum_sq_v_y += pow(var_y, 2)
-        return sum_xy / math.sqrt(sum_sq_v_x * sum_sq_v_y)
-
-    def fit(self, x, y):
-        """
-        Fit and generate model function
-        :param x: list
-        :param y:
-        :return:
-        """
-        m_x = self.mean(x)
-        m_y = self.mean(y)
-        r = self.pearson_r(x, y, m_x, m_y)
-
-        b = r * (self.std(y, m_y) / self.std(x, m_x))
-        a = m_y - b * m_x
-
-        def model(_x):
-            return b * _x + a
-
-        return model
