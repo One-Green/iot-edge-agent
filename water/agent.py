d0570ad0b598330bb0b36bd62bc49c1b185a91a4
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-03-21 18:00:37 +0100

    feat(water/agent.py): WIP reading sensors ok

--- water/agent.py
+++ water/agent.py
@@ -4,8 +4,9 @@ sys.path.insert(0, "..")
 import time
 from core.utils import detect_arduino_usb_serial
 from pyfirmata import ArduinoMega, util
-from input_handler import read_sensors, read_tds, sampling_adc, mean_sample
 from settings import PH_SENSOR_PIN, TDS_SENSOR_PIN, WATER_LEVEL_PIN, NUTRIENT_LEVEL, PH_DOWNER_LEVEL_PIN
+from input_handler import read_sensors
+from pprint import pprint
 
 board = ArduinoMega(detect_arduino_usb_serial())
 it = util.Iterator(board)
@@ -14,11 +15,6 @@ for pin in [PH_SENSOR_PIN, TDS_SENSOR_PIN, WATER_LEVEL_PIN, NUTRIENT_LEVEL, PH_D
     board.analog[pin].enable_reporting()
 
 print(f"[OK] Connected to this board: {board}")
-#
-# while True:
-#     print(mean_sample(board=board, pin=PH_SENSOR_PIN))
-#     #time.sleep(0.5)
-#     #adc = board.analog[PH_SENSOR_PIN].read()
-#     #print(adc)
-#     #time.sleep(0.5)
 
+while True:
+    pprint(read_sensors(board))
