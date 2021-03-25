66c8559c1fe855c45282bd800daac3b63c6d9b1b
shanisma <shanmugathas.vigneswaran@outlook.fr>
2021-03-25 21:37:28 +0100

    feat(water/output_handler.py): digital_output_list add pin name in TUPLE

--- water/output_handler.py
+++ water/output_handler.py
@@ -23,46 +23,53 @@ from pyfirmata import ArduinoMega
 from settings import WATER_PUMP_PIN, NUTRIENT_PUMP_PIN, PH_DOWNER_PUMP_PIN, MIXER_PUMP_PIN
 
 digital_output_list = [
-    ("water_pump_signal", WATER_PUMP_PIN),
-    ("nutrient_pump_signal", NUTRIENT_PUMP_PIN),
-    ("ph_downer_pump_signal", PH_DOWNER_PUMP_PIN),
-    ("mixer_pump_signal", MIXER_PUMP_PIN)
+    ("water_pump_signal", WATER_PUMP_PIN, f'{WATER_PUMP_PIN=}'.split('=')[0]),
+    ("nutrient_pump_signal", NUTRIENT_PUMP_PIN, f'{NUTRIENT_PUMP_PIN=}'.split('=')[0]),
+    ("ph_downer_pump_signal", PH_DOWNER_PUMP_PIN, f'{PH_DOWNER_PUMP_PIN=}'.split('=')[0]),
+    ("mixer_pump_signal", MIXER_PUMP_PIN, f'{MIXER_PUMP_PIN=}'.split('=')[0])
 ]
 
 
-def apply_if_changed(board: ArduinoMega, _callback: dict, _key: str, _pin: int) -> dict:
+def apply_if_changed(
+        board: ArduinoMega,
+        _callback: dict,
+        _key: str,
+        _pin: int,
+        _pin_name: str
+) -> dict:
     """
     Return true if changed
     :param board:
     :param _callback:
     :param _key:
     :param _pin:
+    :param _pin_name:
     :return:
     """
     _state = bool(board.digital[_pin].read())
     _callback_requirement = bool(_callback[_key])
-    pin_name = f'{_pin=}'.split('=')[0]
+
     if _callback_requirement != _state:
         board.digital[_pin].write(_callback[_key])
         return {
-            "pin": pin_name,
+            "pin": _pin_name,
             "changed": True,
             "state": _state
         }
     else:
         return {
-            "pin": pin_name,
+            "pin": _pin_name,
             "changed": False,
             "state": _state
         }
 
 
 def off_on_digital_output(board: ArduinoMega, _callback: dict):
-    for _key, pin in digital_output_list:
-        r = apply_if_changed(
+    for _key, _pin, _pin_name in digital_output_list:
+        yield apply_if_changed(
             board=board,
             _callback=_callback,
             _key=_key,
-            _pin=pin
+            _pin=_pin,
+            _pin_name=_pin_name
         )
-        print(r)
