3afe36378323dbca5defeea6e43bdb1c44bb0e3c
shanisma <shanmugathas.vigneswaran@outlook.fr>
2021-04-01 22:19:54 +0200

    feat(water/state-exporter.py): use falcon api to export

--- water/state-exporter.py
+++ water/state-exporter.py
@@ -1,10 +1,12 @@
-from db import get_db, db_update_or_create
+import falcon
+from core.db import get_state
 
-db = get_db()
 
-db_update_or_create("test", 0.155555545646)
-db_update_or_create("test1", 0.255555545646)
-db_update_or_create("test2", 0.355555545646)
-db_update_or_create("test3", 0.555555545646)
-db_update_or_create("test4", 0.4555555545646)
-db_update_or_create("test", 0.45455555545646)
+class StateView:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.media = get_state()
+
+
+api = falcon.API()
+api.add_route('/state', StateView())
