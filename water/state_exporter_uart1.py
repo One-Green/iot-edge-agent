258ec7939a426a1cff998c946b852b91828c6bd6
shanisma <shanmugathas.vigneswaran@outlook.fr>
2021-07-16 23:59:54 +0200

    feat(water/state_exporter_uart1.py): init for screen

---
+++ water/state_exporter_uart1.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.path.insert(0, "..")
+
+from core.db import get_state
+import simplejson as json
+from datetime import datetime
+import serial
+import time
+
+# Create serial instance
+#  Rx  |  ---> UART1
+#  Tx  |
+#  Gnd
+#  +5V
+#  +5V
+s = serial.Serial("/dev/ttyS1")
+
+while True:
+    d = get_state()
+    d["utc_now"] = datetime.utcnow().isoformat()
+    # python dict to text, use encode() to binary
+    body = json.dumps(d).encode()
+    s.write(body)
+    time.sleep(0.5)
